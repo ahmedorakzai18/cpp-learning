2a89d471fb160442fb44ef41f368e4299eba7246
ahmedorakzai18 <ahmedorakzai18@gmial.com>
2026-07-17 01:41:27 +0500

    Pattern Printing Files

---
+++ Chapter 4(Pattern Printing)/04AlphabateSquare.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std; 
+int main()
+{
+    int m; 
+    cout << "Enter the Numebr: "; 
+    cin >> m; 
+
+    for(int i=1;i<=m; i++)
+    {
+        for(int j=1;j<=m; j++)
+        {
+            cout << (char)(j+96) << " " ;
+        }
+        cout << endl;
+    }
+}

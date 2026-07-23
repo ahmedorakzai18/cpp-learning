32cb909a1a85dbbc591fe1283502c5afec88e4e1
ahmedorakzai18 <ahmedorakzai18@gmail.com>
2026-07-23 11:12:37 +0500

    Pattren Printing files

---
+++ 00 BASICS/Chapter 4(Pattern Printing)/06FloydTriangle.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+ int main()
+{
+    int m ;
+    cout << "Enter the Number: " ; 
+    cin >> m; 
+
+    int a=1; 
+
+    for(int i=1;i<=m;i++)
+    {
+        for(int j=1;j<=i;j++)
+        {
+            cout<< a << " "; 
+            a++;
+        }
+        cout << endl;
+    }
+}

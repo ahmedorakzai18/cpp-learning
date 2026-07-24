fa4f1c66f5bf18310949bf1f0acc6723259dbe90
ahmedorakzai18 <ahmedorakzai18@gmail.com>
2026-07-24 09:17:27 +0500

    Updated 2D files

--- 00 BASICS/Chapter 9(2D Arrays)/00Practice.cpp
+++ 00 BASICS/Chapter 9(2D Arrays)/00Practice.cpp
@@ -1,33 +1,16 @@
-#include <iostream>
+#include<iostream>
 using namespace std; 
 int main()
 {
 
-int r,c; 
-cout << "Enter the ROws; "; 
-cin >> r ; 
+    int arr[3][4]  ={{1,2,3,4},{3,1,4,5},{53,63,62,6}}; 
 
-cout << "Enter the Columns; "; 
-cin >> c ;
-
-int arr[r][c]; 
-
-for(int i=0; i<r; i++)
-{
-    for(int j=0; j<c; j++)
+    for(int i=0; i<3; i++) 
     {
-        cout << "Enter the value for row (" << i << ") and column (" << j << ") : "; 
-        cin >> arr[i][j]; 
+        for(int j=0; j<4; j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl; 
     }
-    cout << endl; 
-}
-
-for(int i=0; i<r; i++)
-{
-    for(int j=0; j<c; j++)
-    {
-        cout << arr[i][j] << " "; 
-    }
-    cout << endl; 
-}
 }

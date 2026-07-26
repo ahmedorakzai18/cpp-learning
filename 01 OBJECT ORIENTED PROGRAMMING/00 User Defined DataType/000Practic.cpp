0b995b92c7cbf168610ec3010a13b9e361ac8ef2
ahmedorakzai18 <ahmedorakzai18@gmail.com>
2026-07-26 07:48:29 +0500

    files

--- 01 OBJECT ORIENTED PROGRAMMING/00 User Defined DataType/000Practic.cpp
+++ 01 OBJECT ORIENTED PROGRAMMING/00 User Defined DataType/000Practic.cpp
@@ -1,22 +1,49 @@
 #include<iostream>
 using namespace std; 
 
-class Greet
+class Student
 {
     public:
+    int roll; 
+    string name; 
+    float gpa;
+    
+    // default constructor
 
-    void hey();
+    Student()
+    {
+        name = "Unknown"; 
+        roll = 0;
+        gpa = 0.00;
+    }
+
+    //parameterized constructor
+
+    Student(string name, int roll, float gpa)
+    {
+        this->name = name; 
+        this->roll = roll; 
+        this->gpa = gpa; 
+    }
+
+        Student(string name, float gpa)
+    {
+        this->name = name; 
+        this->gpa = gpa; 
+    }
+
+    void display()
+    {
+        cout << "Name of the student is: " << name << endl; 
+        cout << "roll of the student is: " << roll << endl; 
+        cout << "gpa of the student is: " << gpa << endl; 
+    }
 };
-
-void Greet::hey()
-{
-    cout << "hello world: "; 
-}
-
 
 int main()
 {
-    Greet obj1; 
-    obj1.hey();
+    Student s1("zia",2,2.5); 
 
+    Student* S2 = new Student("ali",4,12.2) ;  // Dynamic Allocation
+    cout << S2->name; 
 }
---
+++ 01 OBJECT ORIENTED PROGRAMMING/06 Access Modefiers/00Practice.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+using namespace std; 
+
+class Employee
+{
+    public:
+    string name; 
+    int JoinYear; 
+    int OfficeNo;
+    
+};
+
+int main()
+{
+
+}
